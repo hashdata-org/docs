48642a126923d52e42e7bdf5f36be334bdbae97e
Modestino <modestino@gmail.com>
2024-02-01 10:35:04 -0300

    Crowdin integration

--- write-translations.sh
+++ write-translations.sh
@@ -1,5 +1,5 @@
 rm -rf i18n/en i18n/es
 echo "{}" > i18n/pt/docusaurus-plugin-content-docs/current.json
 docusaurus write-translations
-docusaurus write-translations --locale en --override
-docusaurus write-translations --locale es --override
+#docusaurus write-translations --locale en --override
+#docusaurus write-translations --locale es --override
