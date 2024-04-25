1fb637a7a39fdee591f8553908330e6b5b4a5928
Modestino <modestino@gmail.com>
2024-04-25 18:03:40 -0300

    Remoção do Blog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -81,7 +81,7 @@ const config = {
             position: 'left',
             label: 'Docs',
           },
-          {to: '/blog', label: 'Blog', position: 'left'}
+          // {to: '/blog', label: 'Blog', position: 'left'}
         ],
       },
       footer: {
