36215c4182bddf591ca028176905c664647925e9
AdmilsonGoncalves <admilson.goncalves@gmail.com>
2024-11-14 22:21:27 -0300

    HD-T-221 Guia do Usuário - WIP

--- write-translations.sh
+++ write-translations.sh
@@ -1,6 +1,20 @@
+cd ~/IdeaProjects/docs || exit
+
 rm -rf i18n/en i18n/es
 echo "{}" > i18n/pt/docusaurus-plugin-content-docs/current.json
 ./node_modules/.bin/docusaurus write-translations
 
 #./node_modules/.bin/docusaurus write-translations --locale en --override
 #./node_modules/.bin/docusaurus write-translations --locale es --override
+
+# Upload translations, continue even if there are errors
+crowdin upload || echo "continue with errors"
+
+# Download the latest translations
+crowdin download
+
+# Sync specific image files (.png, .webp, .gif) to the English i18n folder
+rsync -avm --include='*.png' --include='*.webp' --include='*.gif' --include='*.css' -f 'hide,! */' docs/ i18n/en/docusaurus-plugin-content-docs/current
+
+# Sync specific image files (.png, .webp, .gif) to the Spanish i18n folder
+rsync -avm --include='*.png' --include='*.webp' --include='*.gif' --include='*.css' -f 'hide,! */' docs/ i18n/es/docusaurus-plugin-content-docs/current
